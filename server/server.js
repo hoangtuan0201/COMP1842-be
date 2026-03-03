dda60808938f5ce8edba2d08fa5d886a73c85e01
PC <hoangtuan0796991061@gmail.com>
2026-03-03 13:03:13 +0700

    vercel deploy

--- server/server.js
+++ server/server.js
@@ -6,8 +6,8 @@ const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 global.Vocab = require('./api/models/vocabModel');
 const routes = require('./api/routes/vocabRoutes');
-    
-    // mongoose instance connection url connection
+
+// mongoose instance connection url connection
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.CONNECTION_STRING);
 
@@ -19,8 +19,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 routes(app); //register the route
-app.listen(port);
-app.use((req, res) => {
-    res.status(404).send({ url: `${req.originalUrl} not found`})
-})
-console.log(`Server is running on port ${port}`);
+
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
+}
+
+module.exports = app;
