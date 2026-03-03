2a0b7b532b6a25ebb20f2d401b7a245bdc0ef338
PC <hoangtuan0796991061@gmail.com>
2026-03-03 13:28:22 +0700

    up

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ const allowedOrigins = [
     'https://comp1842-fe.onrender.com'
 ];
 
-app.use(cors({
+const corsOptions = {
     origin: function (origin, callback) {
         if (!origin || allowedOrigins.indexOf(origin) !== -1) {
             callback(null, true);
@@ -28,8 +28,14 @@ app.use(cors({
             callback(new Error('Not allowed by CORS'));
         }
     },
-    credentials: true
-}));
+    credentials: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204
+};
+
+app.use(cors(corsOptions));
+app.options(/\/.*/, cors(corsOptions)); // enable pre-flight for all routes
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
